492f8242b448f7c7d335e9eb39c7d7b087170efb
legacies56 <mohamedanwer3061@gmail.com>
2025-05-06 18:57:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ document.getElementById("num2-el").textContent = num2
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
-
+let sumEl = doucment.getElementById("sum-el")
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+function add() {
+  sumEl.textContent = num1 + num2
+}
+function subtract() {
+  sumEl.textContent = num1 - num2
+}
+function divide() {
+  sumEl.textContent = num1 / num2
+}
+function multiply() {
+  sumEl.textContent = num1 * num2
+}
 
 
