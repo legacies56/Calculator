72ddf543966707a65fc90ba7b989959aa7afe6a9
legacies56 <mohamedanwer3061@gmail.com>
2025-05-06 19:16:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+
+let num1-el = document.getElementById("num1-el")
+let num2-el = document.getElementById("num2-el")
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 let sumEl = document.getElementById("sum-el")
+
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 function add() {
+  let num1 = praseFloat(num1-el.vaule)
+  let num2 = praseFloat(num2-el.vaule)
   sumEl.textContent = num1 + num2
 }
 function subtract() {
+   let num1 = praseFloat(num1-el.vaule)
+  let num2 = praseFloat(num2-el.vaule)
   sumEl.textContent = num1 - num2
 }
 function divide() {
+   let num1 = praseFloat(num1-el.vaule)
+  let num2 = praseFloat(num2-el.vaule)
   sumEl.textContent = num1 / num2
 }
 function multiply() {
+   let num1 = praseFloat(num1-el.vaule)
+  let num2 = praseFloat(num2-el.vaule)
   sumEl.textContent = num1 * num2
 }
 
